28eb921616daaf2cb2cea276209b543191acd8d8
unilock <unilock@fennet.rentals>
2024-03-24 20:21:55 -0400

    Modify many mixins to allow running on Forge via Sinytra Connector
    Everything SHOULD still function as before on Fabric
    
    Signed-off-by: unilock <unilock@fennet.rentals>

--- src/main/java/com/unascribed/lib39/phantom/mixin/MixinEntity.java
+++ src/main/java/com/unascribed/lib39/phantom/mixin/MixinEntity.java
@@ -8,7 +8,6 @@ import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import com.unascribed.lib39.phantom.quack.PhantomWorld;
 
@@ -66,28 +65,6 @@ public abstract class MixinEntity {
 	public FluidState lib39Phantom$replaceFluidStateForSubmerge(FluidState in) {
 		if (in.isEmpty()) return in;
 		if (world instanceof PhantomWorld && ((PhantomWorld)world).lib39Phantom$isPhased(BlockPos.create(getX(), getEyeY() - 0.1111111119389534, getZ()))) {
-			return Fluids.EMPTY.getDefaultState();
-		}
-		return in;
-	}
-	
-	@ModifyExpressionValue(at=@At(value="INVOKE", target="net/minecraft/util/math/BlockPos$Mutable.set(III)Lnet/minecraft/util/math/BlockPos$Mutable;", ordinal=0),
-			method="updateMovementInFluid")
-	public BlockPos.Mutable lib39Phantom$storeMutableForFluid(BlockPos.Mutable mut) {
-		lib39Phantom$currentlyCollidingPos = mut;
-		return mut;
-	}
-	
-	@Inject(at=@At("RETURN"), method="updateMovementInFluid")
-	public void lib39Phantom$forgetMutableForFluid(CallbackInfoReturnable<Boolean> ci) {
-		lib39Phantom$currentlyCollidingPos = null;
-	}
-	
-	@ModifyExpressionValue(at=@At(value="INVOKE", target="net/minecraft/world/World.getFluidState(Lnet/minecraft/util/math/BlockPos;)Lnet/minecraft/fluid/FluidState;", ordinal=0),
-			method="updateMovementInFluid")
-	public FluidState lib39Phantom$replaceFluidStateForTouch(FluidState in) {
-		if (in.isEmpty()) return in;
-		if (world instanceof PhantomWorld && ((PhantomWorld)world).lib39Phantom$isPhased(lib39Phantom$currentlyCollidingPos)) {
 			return Fluids.EMPTY.getDefaultState();
 		}
 		return in;
